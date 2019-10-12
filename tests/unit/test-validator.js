3d2f217c9f424525f921856d5b873522c178349c
WillemTheWalrus <billdoudna@gmail.com>
2019-10-12 08:50:34 -0700

    starting to work on validator unit tests

--- tests/unit/test-validator.js
+++ tests/unit/test-validator.js
@@ -3,3 +3,8 @@
 const validator = require('../../LayerDependencies/SpotDependencies/nodejs/spotValidator');
 const chai = require('chai');
 const assert = chai.assert;
+
+const invalidJson = {
+  testField: 'fake',
+};
+
